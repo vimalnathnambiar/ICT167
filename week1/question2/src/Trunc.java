1705bfd766836e19a7220c174e74dcb6f7b847b0
Vimalnath Nambiar <vnathnambiar@hotmail.com>
2023-07-30 23:27:56 +0800

    added week 1 solution

---
+++ week1/question2/src/Trunc.java
@@ -0,0 +1,108 @@
+
+// Imported libraries
+import java.util.Scanner;
+
+/*
+ * Title: Lab Week 1 - Question 2
+ * Author: Vimalnath Nambiar
+ * Date: 31-07-2023
+ * File: Trunc.java
+ * 
+ * Purpose: 
+ * - Acquire floating point number
+ * - Display both the number and running total to the nearest whole number
+ * 
+ * Assumptions/Conditions:
+ * - Input to accept a floating point number
+ * - Use loop to receive input
+ * - Output is to display both the number inputted by user and the running total to the nearest whole number
+ * - Uses an out of range number (<-100 or >100) to exit the program
+ */
+
+// Trunc Class
+public class Trunc {
+    // Main Method
+    public static void main(String[] args) {
+        // Initialise scanner object for input
+        Scanner input = new Scanner(System.in);
+
+        // Initialise variables to store total and flag for loop
+        double total = 0;
+        boolean flag = true;
+
+        System.out.println("Use an out of range entry <-100 or >100 to quit.");
+
+        // Start of loop to acquire user input
+        while (flag) {
+            // Retrieve user input using input scanner object
+            // Accepts floating point number
+            System.out.println("Enter a number on a line:");
+            double d = input.nextDouble();
+
+            // Condition to check if d inputted by user falls out of range
+            // If true, set flag = false
+            // Else display d and total to the nearest whole number
+            if (outOfRange(d)) {
+                flag = false;
+            } else {
+                dispWholeNum("The number value is: ", d);
+                total = total + d;
+                dispWholeNum("The total is: ", total);
+
+                System.out.println();
+                System.out.println("Next.");
+            }
+        }
+        System.out.println("You quit.");
+    }
+
+    /*
+     * Method to check if the user inputted number (d) falls out of range
+     */
+    static boolean outOfRange(double d) {
+        // Returns true if d is lesser than -100
+        if (d < -100)
+            return true;
+
+        // Returns true if d is greater than 100
+        if (d > 100)
+            return true;
+
+        // Returns false if d does not meet the above two conditions
+        return false;
+
+        // Can also be written as follows:
+        // return d < -100 || d > 100;
+    }
+
+    /*
+     * Method to display on screen the message msg
+     * Followed by num correct to the nearest whole number
+     */
+    static void dispWholeNum(String msg, double num) {
+        // Record whether num is negative
+        boolean neg = (num < 0);
+
+        // Make a positive version of the number
+        double posNum = num;
+        if (neg)
+            posNum = -num;
+
+        // Add 0.5 to the posNum, so that truncating nPlus
+        // is equivalent to rounding posNum
+        double nPlus = posNum + 0.5;
+
+        // Extract the whole number part of the number
+        int whole = (int) nPlus;
+
+        // Assign negative sign to whole number if num was a negative
+        String sign = "";
+        if (neg)
+            sign = "-";
+        // whole = -whole;
+
+        // Display the message, sign, the whole number
+        System.out.println(msg + "" + sign + whole);
+        // System.out.println(msg + "" + whole);
+    }
+}
---
+++ week1/question3/src/VowelCopy.java
@@ -0,0 +1,159 @@
+
+// Imported libraries
+import java.util.Scanner;
+
+/*
+ * Title: Lab Week 1 - Question 3
+ * Author: Vimalnath Nambiar
+ * Date: 31-07-2023
+ * File: StringReader.java
+ * 
+ * Purpose: 
+ * - Read string from keyboard
+ * - Copy characters from the strings that are vowels into an array
+ * - Display the copied characters from the array
+ * - Count and display number of times each vowel appears
+ * - Output the index in the array where each vowel first appeared or display suitable message if a particular vowel is not in the array
+ * 
+ * Assumptions/Conditions:
+ * - String needs to be a maximum length of 30
+ * - Characters (only vowels) are to be copied into the array and should be in order of input
+ * - Display of copied characters should be in order of input
+ * - Only the index where each vowel first appeared should be displayed
+ */
+
+// StringReader Class
+public class VowelCopy {
+    // Main Method
+    public static void main(String[] args) {
+        // Initialise scanner object for input
+        Scanner input = new Scanner(System.in);
+
+        // Initialise variables
+        boolean flag = true;
+
+        // Start loop
+        while (flag) {
+            // Receive string (message) from user input
+            System.out.println("Enter a string to a maximum length of 30:");
+            String inputString = input.nextLine();
+
+            // Check if input string length is within the maximum length of 30
+            if (inputString.length() <= 30) {
+
+                // Extract vowels from input string into an array
+                char[] vowelArray = extractVowel(inputString);
+
+                // Display vowel characters stored in the array acquired from the input string
+                System.out.println("\nThe output of the second array is:");
+                System.out.println(vowelArray);
+
+                // Display number of counts for each vowel
+                System.out.println("\nThe counts are as follows:");
+                countVowel(vowelArray);
+
+                // Display index of the first occurence of the vowel in the array
+                System.out.println("\nThe index of the second array where each vowel first appeared:");
+                displayFirstIndex(vowelArray);
+
+                // Set flag to false to exit loop
+                flag = false;
+
+            } else {
+                System.out.println("\nThe string is too long. Please enter again.\n");
+            }
+        }
+        System.out.println("\nExiting the program now\n");
+    }
+
+    /*
+     * Method to extract vowel characters from the input string
+     */
+    static char[] extractVowel(String inputString) {
+        // Initialise a second character array to store character vowels
+        char[] vowelArray = new char[inputString.length()];
+        int index = 0;
+
+        // Loop through input string
+        for (int i = 0; i < inputString.length(); i++) {
+            char ch = inputString.charAt(i);
+            ch = Character.toLowerCase(ch);
+
+            // If character is a vowel, add it to array
+            if (isVowel(ch)) {
+                vowelArray[index++] = ch;
+            }
+        }
+        return vowelArray;
+    }
+
+    /*
+     * Method to check if a character is a vowel
+     */
+    static boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+
+    /*
+     * Method to count and display the number of times a vowel appears in an array
+     */
+    static void countVowel(char[] vowelArray) {
+        int aCounter = 0;
+        int eCounter = 0;
+        int iCounter = 0;
+        int oCounter = 0;
+        int uCounter = 0;
+
+        // Count the number of times each vowel is present in the array
+        for (int i = 0; i < vowelArray.length; i++) {
+            switch (vowelArray[i]) {
+                case 'a':
+                    aCounter++;
+                    break;
+                case 'e':
+                    eCounter++;
+                    break;
+                case 'i':
+                    iCounter++;
+                    break;
+                case 'o':
+                    oCounter++;
+                    break;
+                case 'u':
+                    uCounter++;
+                    break;
+            }
+        }
+
+        // Display the vowel count
+        System.out.printf("a = %d\ne = %d\ni = %d\no = %d\nu = %d\n",
+                aCounter, eCounter, iCounter, oCounter, uCounter);
+    }
+
+    /*
+     * Method to display the first occurence of a vowel in an array
+     */
+    static void displayFirstIndex(char[] vowelArray) {
+        // A array of type character that stores the list of vowels
+        char[] vowel = new char[] { 'a', 'e', 'i', 'o', 'u' };
+
+        // Nested loop to loop check each character in the vowel array against a vowel
+        for (int i = 0; i < vowel.length; i++) {
+            boolean vowelFound = false;
+
+            for (int j = 0; j < vowelArray.length; j++) {
+                // Display index of the first occurence of a vowel in the array
+                if (vowelArray[j] == vowel[i]) {
+                    System.out.println(vowel[i] + " = " + j);
+                    vowelFound = true;
+                    break;
+                }
+            }
+            // Display suitable message if vowel is not found in the array
+            if (!vowelFound) {
+
+                System.out.println(vowel[i] + " is not in the input");
+            }
+        }
+    }
+}
