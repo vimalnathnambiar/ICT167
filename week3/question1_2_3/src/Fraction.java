a06dba20db2a8583ade4ab1d1c675379f377807b
Vimalnath Nambiar <vnathnambiar@hotmail.com>
2023-08-13 23:02:42 +0800

    added week 3

---
+++ week3/question1_2_3/src/Fraction.java
@@ -0,0 +1,102 @@
+/*
+ * Title: Lab Week 3 - Question 1, 2, and 3
+ * Author: Vimalnath Nambiar
+ * Date: 13-08-2023
+ * File: Fraction.java
+ * 
+ * Purpose: 
+ * - Designed to handle fractions
+ * 
+ * Assumptions/Conditions:
+ * Question 1
+ * ---
+ * - Contains two instance variables
+ *      - numerator (int) and denominator (int)
+ *      - publicly accessible
+ * - Should never store a value of 0 for the denominator
+ * - Supply an output method to print out a fraction (as two separate integers) (numerator / denominator)
+ * 
+ * Question 3
+ * ---
+ * - Ensure instance variables are hidden
+ * - Implement appropriate accessor and mutator methods
+ * 
+ * 
+ * Other:
+ * - Write pseudocode before coding the class and the client program
+ * - Understand why your client class can't compile when instance variable is switched to hidden
+ */
+
+// Fraction class
+public class Fraction {
+    // // Two instance variables called numerator and denominator
+    // // that are publicly available
+    // public int numerator;
+    // public int denominator;
+
+    // Two instance variables called numerator and denominator
+    // that are hidden
+    private int numerator;
+    private int denominator;
+
+    // Constructors
+    public Fraction() {
+
+    }
+
+    public Fraction(int numerator, int denominator) {
+        // Throw an error if denominator value entered is 0
+        if (denominator == 0) {
+            throw new IllegalArgumentException("Denominator cannot be zero");
+        }
+
+        // Check denominator input
+        // If negative, swap sign of both numerator and denominator instance variables
+        if (denominator < 0) {
+            numerator = -numerator;
+            denominator = -denominator;
+        }
+
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    // Mutator methods
+    // Set numerator value
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    // Set denominator value
+    // Also checks if the denominator = 0 or negative value
+    public void setDenominator(int denominator) {
+        // Throw an error if denominator value entered is 0
+        if (denominator == 0) {
+            throw new IllegalArgumentException("Denominator cannot be zero");
+        }
+
+        // Check denominator input
+        // If negative, swap sign of both numerator and denominator instance variables
+        if (denominator < 0) {
+            setNumerator(-numerator);
+            denominator = -denominator;
+        }
+        this.denominator = denominator;
+    }
+
+    // Accessor Methods
+    // Returns numerator value
+    public int getNumerator() {
+        return numerator;
+    }
+
+    // Return denominator value
+    public int getDenominator() {
+        return denominator;
+    }
+
+    // Output fraction in the form of numerator / denominator
+    public void outputFraction() {
+        System.out.println("Fraction: " + this.numerator + " / " + this.denominator + "\n");
+    }
+}
---
+++ week3/question1_2_3/src/TestFraction.java
@@ -0,0 +1,75 @@
+/*
+ * Title: Lab Week 3 - Question 1, 2, and 3
+ * Author: Vimalnath Nambiar
+ * Date: 13-08-2023
+ * File: TestFraction.java
+ * 
+ * Purpose: 
+ * - A client class to use Fraction class methods or instance variables to get inputs and display fractions
+ * 
+ * Assumptions/Conditions:
+ * Question 1
+ * ---
+ * - Use a loop for getting fractions
+ * - Stop loop when a negative value entered for the numerator
+ * 
+ * Question 2
+ * ---
+ * - Ensure input values representing fractions are stored with denominators that are positive integers
+ * - User should not be restricted to only enter positive integer
+ * - Swap the sign of both numerator and denominator instance variables
+ * 
+ * Other:
+ * - Write pseudocode before coding the class and the client program
+ */
+
+// Import libraries/dependencies
+import java.util.*;
+
+// TestFraction class
+public class TestFraction {
+    // Main method
+    public static void main(String[] args) {
+
+        // Initialise scanner object to retrieve user input
+        Scanner kb = new Scanner(System.in);
+
+        // Create an object instance of class Fraction
+        // frac is an object referencing to class Fraction
+        Fraction frac = new Fraction();
+
+        while (true) {
+            // Retrieve user input for numerator and denominator value
+            System.out.print("Enter the numerator value: ");
+            int numerator = kb.nextInt();
+
+            // Check if a negative value was inputted by user for numerator
+            // If negative, break out of loop to exit program
+            if (numerator < 0) {
+                System.out.println("\nExiting program now");
+                break;
+            }
+
+            System.out.print("Enter the denominator value: ");
+            int denominator = kb.nextInt();
+
+            // // Create an object instance of class Fraction
+            // // frac is an object referencing to class Fraction
+            // Fraction frac = new Fraction(numerator, denominator);
+
+            // Set numerator and denominator values to object frac
+            frac.setNumerator(numerator);
+            frac.setDenominator(denominator);
+
+            // Get numerator and denominator values stored in the object reference
+            System.out.println("Numerator stored: " + frac.getNumerator());
+            System.out.println("Denominator stored: " + frac.getDenominator());
+
+            // Output fraction
+            frac.outputFraction();
+        }
+        // Close scanner object
+        kb.close();
+    }
+
+}
